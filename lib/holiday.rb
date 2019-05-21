6cb617c68d95cc57a3c3a1538f85016513a47915
Ashton Williams <ashtonwilliams1996@gmail.com>
2019-05-21 21:58:18 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -85,7 +85,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season , holiday|
     holiday.each do |actual_holiday , decor|
       if decor.include?("BBQ")
-        puts season
+        puts actual_holiday
       end
     end
   end
